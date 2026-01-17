0a7828664fcbb46d54818a2d8b6de1faf932bfb6
fithajabar03-art <fithajabar03@gmail.com>
2026-01-17 20:29:07 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+name="fitha"
+role="intern"
+today_date="17-01-2026"
+print("name:",name)
+print("role:",role)
+print("date",today_date)
+name=input("enter your name:")
+role=input("enter your internship role:")
+today_date="17-01-2026"
+print("\n---user details---")
+print("name:",name)
+print("role:",role)
+print("date:",today_date)
